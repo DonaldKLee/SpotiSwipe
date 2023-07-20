3016388b22419569be17b3b3db687867fedd649a
DonaldKLee <Donald3010@outlook.com>
2023-07-20 11:32:57 -0700

    displays songs

--- dev/src/pages/startswiping.jsx
+++ dev/src/pages/startswiping.jsx
@@ -10,10 +10,25 @@ const StartSwipingPageContainer = styled.div`
 `
 
 const SwipeContainer = styled.div`
-
+    width: 1000%;
 `
 
 const MusicContainer = styled.div`
+    background: #333;
+    height: 400px;
+    width: 250px;
+    border-radius: 20px;
+    display: inline-block;
+    margin: 10px;
+    overflow: hidden;
+`
+
+const MusicImage = styled.img`
+    height: 240px;
+    width: 100%;
+`
+
+const MusicName = styled.h3`
 `
 
 // DOCS https://jmperezperez.com/spotify-web-api-js/
@@ -25,6 +40,7 @@ const StartSwipingPage = () => {
     const [accessTokenURL, setAccessTokenURL] = useState("")
     const [spotifyToken, setSpotifyToken] = useState("")
     const [dataTest, setTestData] = useState("")
+    const [songIDs, setSongIDs] = useState([])
     const [songsToDisplay, setDisplaySongs] = useState([])
     const location = useLocation()
     const selectedPlaylists = location.state.playlists
@@ -44,7 +60,7 @@ const StartSwipingPage = () => {
             spotify.setAccessToken(userSpotifyToken)
         }
         if (selectedPlaylists) {
-            const songsInPlaylists = []
+            let songsInPlaylists = [];
             // get multiple albums
             for (const playlist of selectedPlaylists) {
                 // console.log(playlist.id)
@@ -52,49 +68,89 @@ const StartSwipingPage = () => {
                     function (playlistInfo) {
                         for (const song of playlistInfo.tracks.items) {
                             if (!(songsInPlaylists.includes(song.track.id))) {
+                                // console.log(song.track.id)
                                 songsInPlaylists.push(song.track.id)
                             }
                         }
+                        // Updates songsID state
+                        setSongIDs(songsInPlaylists)
                     },
                     function (err) {
                         alert("ERROR")
                     }
                 );
             }
-    
-            console.log(songsInPlaylists)
-            Object.keys(songsInPlaylists).map(function(key) {
-                console.log(songsInPlaylists[key])
-                console.log("AHAHAHHA")
-            });
-    
-            const seeds = {
-                seed_tracks: "2vemyNxH2DoqQ6e2lOwSzS"
-            }
-    
-            spotify.getRecommendations(seeds).then(
-                function (data) {
-                    setDisplaySongs(data.tracks)
-                },
-                function (err) {
-                    console.error(err);
-                })
         }
     }, [spotifyToken])
+
+    // runs if songIDs is updated
+    useEffect(() => {
+        let recommendations = [];
+        // console.log(songIDs[0])
+        // console.log({songsInPlaylists}["songsInPlaylists"]["0"])
+        const randomlySelectedSongs = []
+        for (let i = 0; i<5; i++) {
+            randomlySelectedSongs.push(songIDs[Math.floor(Math.random() * songIDs.length)]);
+        }
+
+        for (const songID of randomlySelectedSongs) {
+            const seeds = {
+                seed_tracks: songID
+            }
+
+            // waits for all requests to be finished before updating the state
+            const fetchRecommendations = async () => {
+                const promises = randomlySelectedSongs.map((songID) => {
+                  const seeds = {
+                    seed_tracks: songID,
+                  };
+          
+                  return spotify.getRecommendations(seeds);
+                });
+          
+                try {
+                  const responses = await Promise.all(promises);
+                  const recommendations = responses.flatMap((data) => data.tracks);
+                  setDisplaySongs(recommendations);
+                } catch (err) {
+                  console.error(err);
+                }
+              };
+          
+              fetchRecommendations();
+        }
+
+        // console.log(recommendations)
+        setDisplaySongs(recommendations)
+    }, [songIDs])
+
+    useEffect(() => {
+        console.log(songsToDisplay)
+    }, [songsToDisplay])
 
     return (
         <StartSwipingPageContainer>
             <Navbar />
             <br /><br /><br /><br />
             <SwipeContainer>
-            {songsToDisplay && songsToDisplay.map((song, index) => (
-                <MusicContainer key={index}>
-                    <p style={{color:"white"}}>{ song["name"] }</p>
-                </MusicContainer>
-            ))}
-          
+                {/* <h1>{Object.keys(songsToDisplay).length}</h1> */}
+                {Object.keys(songsToDisplay).length > 0 ? (
+                    songsToDisplay.map((song, index) => (
+                    <MusicContainer key={index}>
+                        <MusicImage src={song["album"]["images"][0].url} />
+                        <MusicName style={{ color: "white" }}>{song["name"]}</MusicName>
+
+                        <audio controls>
+                        <source src={song["preview_url"]} type="audio/ogg" />
+                        Your browser does not support the audio element.
+                        </audio>
+                    </MusicContainer>
+                    ))
+                ) : (
+                    <p>No songs to display</p>
+                )}
             </SwipeContainer>
-            <h1>Swipe!</h1>
+      
         </StartSwipingPageContainer>
     )
 }
